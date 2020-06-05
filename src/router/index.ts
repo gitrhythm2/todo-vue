11a5e3fd9662ac6f52d443e0cd3edf3354276fc8
細川弘幸 <hiro@gitrhythm.local>
2020-06-05 12:25:11 +0900

    Home画面をTodo画面に差し替え

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import TodoView from '../views/TodoView.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Todo',
+    component: TodoView
   },
   {
     path: '/about',
@@ -16,7 +16,7 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
---
+++ src/views/TodoView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Todoリスト</h1>
+    <p>This is the todo view template.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+export default class TodoView extends Vue {
+}
+</script>
