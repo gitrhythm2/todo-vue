588ed445e93a732f53a1ea8fcb397e3aa2bef878
Hiroyuki Hosokawa <hiro@gitrhythm.local>
2020-06-13 21:47:11 +0900

    #1 リファクタリング：firebaseアクセス回りのロジックをTodoRepositoryクラスとしてまとめた

--- src/models/TodoModel.ts
+++ src/models/TodoModel.ts
@@ -2,10 +2,13 @@
 import { Todo } from '@/types/Todo'
 import firebase, { firestore } from 'firebase/app'
 import 'firebase/firestore'
+import TodoRepository from '@/repositories/TodoRepository'
 
 type TodoSnapshot = firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>
 
 export default class TodoModel {
+  private todoRepo = new TodoRepository()
+
   public todos: Todo[]
 
   options = [
@@ -19,21 +22,7 @@ export default class TodoModel {
   }
 
   async fetch (condition: number): Promise<void> {
-    let snapShot: TodoSnapshot
-    if (condition === -1) {
-      snapShot = await this.findAll()
-    } else {
-      snapShot = await this.findByCondition(condition)
-    }
-
-    const newTodos: Todo[] = []
-    snapShot.docs.forEach(doc => {
-      const todo: Todo = doc.data() as Todo
-      todo.id = doc.id
-      // console.log('id:' + todo.id + 'title:' + todo.title + ' - state:' + todo.state)
-      newTodos.push(todo)
-    })
-    this.todos = newTodos
+    this.todos = await this.todoRepo.fetch(condition)
   }
 
   async add (title: string): Promise<number | void> {
@@ -50,11 +39,7 @@ export default class TodoModel {
       createdAt: firebase.firestore.FieldValue.serverTimestamp()
     }
 
-    this.collection()
-      .add(newTodo)
-      .catch(error => {
-        console.error('Error adding document: ', error)
-      })
+    await this.todoRepo.add(newTodo)
   }
 
   async changeState (todo: Todo): Promise<void> {
@@ -64,42 +49,16 @@ export default class TodoModel {
     }
     this.todos[index].done = !this.todos[index].done
 
-    this.collection()
-      .doc(todo.id)
-      .update(this.todos[index])
+    await this.todoRepo.update(todo)
   }
 
   async remove (todo: Todo): Promise<void> {
-    await this.collection()
-      .doc(todo.id)
-      .delete()
+    await this.todoRepo.remove(todo)
 
     const index = this.todos.indexOf(todo)
     if (index === -1) {
       throw new Error('TodoModel:remove todo not found.')
     }
     this.todos.splice(index, 1)
-  }
-
-  private collection () {
-    return firebase.firestore().collection('todos')
-  }
-
-  private findAll (): Promise<TodoSnapshot> {
-    return this.collection()
-      .orderBy('createdAt', 'desc')
-      .get()
-  }
-
-  private findByCondition (condition: number): Promise<TodoSnapshot> {
-    if (condition !== 0 && condition !== 1) {
-      throw new Error('Bad argument[condition]')
-    }
-
-    const done = (condition === 1)
-    return this.collection()
-      .orderBy('createdAt', 'desc')
-      .where('done', '==', done)
-      .get()
   }
 }
---
+++ src/repositories/TodoRepository.ts
@@ -0,0 +1,73 @@
+
+import { Todo } from '@/types/Todo'
+import firebase, { firestore } from 'firebase/app'
+import 'firebase/firestore'
+
+type TodoSnapshot = firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>
+
+export default class TodoRepository {
+  async fetch (condition: number): Promise<Todo[]> {
+    let snapShot: TodoSnapshot
+    if (condition === -1) {
+      snapShot = await this.findAll()
+    } else {
+      snapShot = await this.findByCondition(condition)
+    }
+
+    const newTodos: Todo[] = []
+    snapShot.docs.forEach(doc => {
+      const todo: Todo = doc.data() as Todo
+      todo.id = doc.id
+      // console.log('id:' + todo.id + 'title:' + todo.title + ' - state:' + todo.state)
+      newTodos.push(todo)
+    })
+
+    return newTodos
+  }
+
+  async add (todo: any): Promise<void> {
+    if (todo === null) {
+      throw new Error('todo is null.')
+    }
+
+    this.collection()
+      .add(todo)
+      .catch(error => {
+        throw error
+      })
+  }
+
+  async update (todo: Todo): Promise<void> {
+    this.collection()
+      .doc(todo.id)
+      .update(todo)
+  }
+
+  async remove (todo: Todo): Promise<void> {
+    await this.collection()
+      .doc(todo.id)
+      .delete()
+  }
+
+  private collection () {
+    return firebase.firestore().collection('todos')
+  }
+
+  private findAll (): Promise<TodoSnapshot> {
+    return this.collection()
+      .orderBy('createdAt', 'desc')
+      .get()
+  }
+
+  private findByCondition (condition: number): Promise<TodoSnapshot> {
+    if (condition !== 0 && condition !== 1) {
+      throw new Error('Bad argument[condition]')
+    }
+
+    const done = (condition === 1)
+    return this.collection()
+      .orderBy('createdAt', 'desc')
+      .where('done', '==', done)
+      .get()
+  }
+}
